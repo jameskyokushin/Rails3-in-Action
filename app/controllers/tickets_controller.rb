19b74562812482a61bc01712e158dc7df23c141d
jameskyokushin <james@kyokushin.com.ph>
2012-03-20 11:16:10 +0800

    implementd deleting tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -35,6 +35,12 @@ class TicketsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@ticket.destroy
+		flash[:notice] = "Ticket has been deleted."
+		redirect_to @project
+	end
+
 	def find_ticket
 		@ticket = @project.tickets.find(params[:id])
 	end
